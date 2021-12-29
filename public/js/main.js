80853ad7d35de699ffa61a5aa3fe451470d81f19
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-12-30 03:32:53 +0530

    feat: add ping pong game
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-const W = 17, H = 17, CELL_SIZE = 20;
+const W = 17, H = 17, CELL_SIZE = 20, TEXT_FONT = '16px serif';
 let HIDDEN_CANVAS = null;
 let EVENT_COUNTER = 0;
 
@@ -49,13 +49,24 @@ function rearrange(status) {
     }
 }
 
+function get_text_length(text) {
+    const c = HIDDEN_CANVAS;
+    const x = c.getContext("2d");
+    x.save();
+    x.fillStyle = 'white';
+    x.font = TEXT_FONT;
+    const text_metrics = x.measureText(text);
+    x.restore();
+    return text_metrics.width;
+}
+
 function render_text(text, text_x, text_y, text_color = 'white') {
     const c = HIDDEN_CANVAS;
     const x = c.getContext("2d");
     x.fillStyle = "black";
     x.fillRect(0, 0, W, H);
     x.fillStyle = text_color;
-    x.font = "22px serif";
+    x.font = TEXT_FONT;
     x.fillText(text, text_x, text_y);
     const rgba = x.getImageData(0, 0, W, H).data;
     const rgb = new Uint8Array(W * H * 3);
@@ -69,14 +80,14 @@ function render_text(text, text_x, text_y, text_color = 'white') {
 }
 
 function render_scrolling_text(event_idx, text, text_color = 'white', t_speed = 100) {
-    const min_text_x = -8 * text.length, max_text_x = 0;
+    const min_text_x = -get_text_length(text), max_text_x = 10;
     let last_t = 0, text_x = max_text_x, text_step = -1;
     function draw(t) {
         if (EVENT_COUNTER !== event_idx) return;
         if (last_t === 0) last_t = t;
         if (t - last_t < t_speed) return requestAnimationFrame(draw);
         last_t = t;
-        const new_status = render_text(text, text_x, H, text_color);
+        const new_status = render_text(text, text_x, H - 4, text_color);
         fetch('/display-image', { method: 'PUT', headers: { 'Content-Type': 'application/octet-stream' }, body: new_status }).catch(console.error);
         text_x += text_step;
         if (text_x <= min_text_x || text_x >= max_text_x) text_step *= -1;
@@ -98,13 +109,6 @@ function get_rgb_from_canvas_ctx(x) {
 
 function send_image_to_display(rgb) {
     fetch('/display-image', { method: 'PUT', headers: { 'Content-Type': 'application/octet-stream' }, body: rgb }).catch(console.error);
-}
-
-function show_clock() {
-    const rgb = new Uint8Array(W * H * 3);
-    render_clock(rgb, Math.floor(W / 2), Math.floor(H / 2), 5);
-    rearrange(rgb);
-    send_image_to_display(rgb);
 }
 
 function render_clock(rgb, cen_x, cen_y, radius) {
@@ -171,10 +175,140 @@ function render_canvas_clock(event_idx) {
         x.restore();
         const rgb = get_rgb_from_canvas_ctx(x);
         rearrange(rgb);
-        send_image_to_display(rgb);    
+        send_image_to_display(rgb);
         requestAnimationFrame(draw);
     }
 
+    requestAnimationFrame(draw);
+}
+
+function show_ping_pong(event_idx) {
+    const c = HIDDEN_CANVAS;
+    const x = c.getContext("2d");
+    const PLAYER_LEN = 4;
+    const STATE = {
+        p1_y: 0,
+        p2_y: H - PLAYER_LEN,
+        ball_x: Math.floor(W / 2),
+        ball_y: Math.floor(H / 2),
+        ball_vx: 1,
+        ball_vy: 1,
+        p1_score: 4,
+        p2_score: 4,
+    };
+    function handle_ping_pong(e) {
+        if (e.key === "w") {
+            STATE.p1_y -= 1;
+            if (STATE.p1_y < 0) STATE.p1_y = 0;
+        } else if (e.key === "s") {
+            STATE.p1_y += 1;
+            if (STATE.p1_y >= H - PLAYER_LEN) STATE.p1_y = H - PLAYER_LEN;
+        } else if (e.key === "ArrowUp") {
+            STATE.p2_y -= 1;
+            if (STATE.p2_y < 0) STATE.p2_y = 0;
+        } else if (e.key === "ArrowDown") {
+            STATE.p2_y += 1;
+            if (STATE.p2_y >= H - PLAYER_LEN) STATE.p2_y = H - PLAYER_LEN;
+        }
+    }
+    document.body.addEventListener("keydown", handle_ping_pong);
+    const t_speed = 100;
+    let last_t = null;
+    function draw(t) {
+        if (EVENT_COUNTER !== event_idx) {
+            return document.body.removeEventListener("keydown", handle_ping_pong);
+        }
+        if (STATE.p1_score === 0 || STATE.p2_score === 0) {
+            return render_scrolling_text(event_idx, STATE.p1_score === 0 ? 'blue wins!' : 'red wins!', STATE.p1_score === 0 ? 'blue' : 'red');
+        }
+        if (last_t === null) last_t = t;
+        if (t - last_t < t_speed) return requestAnimationFrame(draw);
+        last_t = t;
+        x.save();
+        x.fillStyle = "black";
+        x.fillRect(0, 0, W, H);
+        x.fillStyle = "red";
+        x.fillRect(0, STATE.p1_y, 1, PLAYER_LEN);
+        x.fillStyle = "blue";
+        x.fillRect(W - 1, STATE.p2_y, 1, PLAYER_LEN);
+        // net
+        let i = 0;
+        for (let r_lives = STATE.p1_score; r_lives > 0 && i < H; i += 2) {
+            if (i === Math.floor(W / 2)) {
+                x.fillStyle = 'white';
+                x.fillRect(Math.floor(W / 2), i, 1, 1);
+                continue;
+            }
+            x.fillStyle = 'red';
+            x.fillRect(Math.floor(W / 2), i, 1, 1);
+            r_lives--;
+        }
+        for (let b_lives = STATE.p2_score; b_lives > 0 && i < H; i += 2) {
+            if (i === Math.floor(W / 2)) {
+                x.fillStyle = 'white';
+                x.fillRect(Math.floor(W / 2), i, 1, 1);
+                continue;
+            }
+            x.fillStyle = 'blue';
+            x.fillRect(Math.floor(W / 2), i, 1, 1);
+            b_lives--;
+        }
+        // ball
+        x.fillStyle = "white";
+        x.fillRect(STATE.ball_x, STATE.ball_y, 1, 1);
+        x.restore();
+        // update state
+        STATE.ball_x += STATE.ball_vx;
+        STATE.ball_y += STATE.ball_vy;
+        // collision
+        if (
+            STATE.ball_x === 1 &&
+            STATE.ball_y >= STATE.p1_y &&
+            STATE.ball_y < STATE.p1_y + PLAYER_LEN
+        ) {
+            STATE.ball_vx *= -1;
+        } else if (
+            STATE.ball_x === W - 2 &&
+            STATE.ball_y >= STATE.p2_y &&
+            STATE.ball_y < STATE.p2_y + PLAYER_LEN
+        ) {
+            STATE.ball_vx *= -1;
+        } else if (
+            STATE.ball_x === 1 &&
+            STATE.ball_y + STATE.ball_vy >= STATE.p1_y &&
+            STATE.ball_y + STATE.ball_vy < STATE.p1_y + PLAYER_LEN
+        ) {
+            STATE.ball_vx *= -1;
+            STATE.ball_vy *= -1;
+        } else if (
+            STATE.ball_x === W - 2 &&
+            STATE.ball_y + STATE.ball_vy >= STATE.p2_y &&
+            STATE.ball_y + STATE.ball_vy < STATE.p2_y + PLAYER_LEN
+        ) {
+            STATE.ball_vx *= -1;
+            STATE.ball_vy *= -1;
+        }
+
+        if (STATE.ball_y === 0 || STATE.ball_y === H - 1) {
+            STATE.ball_vy *= -1;
+        }
+        // out of bounds
+        if (
+            STATE.ball_x < 0 ||
+            STATE.ball_x >= W ||
+            STATE.ball_y < 0 ||
+            STATE.ball_y >= H
+        ) {
+            if (STATE.ball_x < 0) { STATE.p1_score--; STATE.p2_score++; }
+            else if (STATE.ball_x >= W) { STATE.p1_score++; STATE.p2_score--; }
+            STATE.ball_x = Math.floor(W / 2);
+            STATE.ball_y = Math.floor(H / 2);
+        }
+        const rgb = get_rgb_from_canvas_ctx(x);
+        rearrange(rgb);
+        send_image_to_display(rgb);
+        return requestAnimationFrame(draw);
+    }
     requestAnimationFrame(draw);
 }
 
@@ -235,8 +369,12 @@ async function main() {
     const controls_button_clock = document.querySelector('#controls-button-clock');
     controls_button_clock.addEventListener('click', () => {
         ++EVENT_COUNTER;
-        // show_clock();
         render_canvas_clock(EVENT_COUNTER);
+    });
+    const controls_button_ping_pong = document.querySelector('#controls-button-ping-pong');
+    controls_button_ping_pong.addEventListener('click', () => {
+        ++EVENT_COUNTER;
+        show_ping_pong(EVENT_COUNTER);
     });
     console.log('main end');
 }
